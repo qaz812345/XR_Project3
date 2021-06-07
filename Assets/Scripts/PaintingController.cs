2394353903219e42c30e79e3595abc22128949e9
chtruiBen <hsiang0418@gmail.com>
2021-06-08 05:34:16 +0800

    addStarryNight1/UI

--- Assets/Scripts/PaintingController.cs
+++ Assets/Scripts/PaintingController.cs
@@ -5,17 +5,22 @@ using UnityEngine.UI;
 
 public class PaintingController : MonoBehaviour
 {
+    public GameObject startUI;
+    public GameObject ipad;
     public List<Material> skyboxes;
     public List<Painting> correct_paintings;
     public List<Painting> incorrect_paintings;
     public List<GameObject> paintings;
     public GameObject resultUI;
+    
     public Text resultText, titleText, titleENText, authorText, yearText, infoText;
     public GameObject correct_borad, incorrect_borad;
     private int correct_counter = 0;
 
     void Start()
     {
+        Destroy(startUI,4.0f);
+        ipad.SetActive(false);
         resultUI.SetActive(false);
         correct_borad.SetActive(false);
         incorrect_borad.SetActive(false);
@@ -55,11 +60,24 @@ public class PaintingController : MonoBehaviour
         }
     }
 
-    public void close()
+    public void close(int butt)
     {
-        resultUI.SetActive(false);
+        int cls = butt;
+        if(cls == 1)
+                startUI.SetActive(false);
+        else    
+                resultUI.SetActive(false);
+          
     }
+    public void open(int butt)
+    {
+        int op = butt;
+        if (op == 1)
+            ipad.SetActive(true);
+        else
+            ipad.SetActive(false);
 
+    }
     private void SetSkybox(int no)
     {
         RenderSettings.skybox = skyboxes[no];
